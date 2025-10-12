abd8c73b69c42348cb34f382490885c8b68d0162
Vishal_0312 <vishalthonukunooru@gmail.com>
2025-10-12 17:37:37 +0530

    rock choice done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const computerchoice=Math.random();
-let resultOfComputer;
-if(computerchoice>=0 && computerchoice<1/3){
-  resultOfComputer="Rock";
-}else if(computerchoice>=1/3 && computerchoice<2/3){
-  resultOfComputer="Paper";
-}else{
-  resultOfComputer="Scissors";
+function computerchoice(){
+    const computerchoice=Math.random();
+    let resultOfComputer;
+    if(computerchoice>=0 && computerchoice<1/3){
+      resultOfComputer="Rock";
+    }else if(computerchoice>=1/3 && computerchoice<2/3){
+      resultOfComputer="Paper";
+    }else{
+      resultOfComputer="Scissors";
+    }
 }
+
